3cf217b7b790fb770c03780bc6906643d9df93b9
t243 <mulenzitonny@gmai.com>
2020-07-25 20:16:16 +0300

    upgraded pages

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,13 @@
 function myFunction() {
     alert("Thank you for contacting me, will get back soon");
   }
+  //Opens a side menue to links
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+}
